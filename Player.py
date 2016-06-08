cccc95f5025833792e912d504607e2b50be5be24
IT Ích <IT ch>
2016-06-08 20:48:38 +0700

    test

---
+++ Player.py
@@ -0,0 +1,19 @@
+import random
+class player:
+	__x
+	__y
+	def __init__():
+		
+		self.x = random.randint(0, 4)
+		self.y = random.randint(0, 4)
+	def __init__(x,y):
+		self.x = x
+		self.y = y	
+	def getX():
+		return player.x
+	def getY():
+		return player.y
+	def setX(px):
+		player.x =px
+	def setY():
+		player.y =px
---
+++ game.py
@@ -0,0 +1,13 @@
+map = [['1','2','3','4'],['5','6','7','8']]
+def init():
+	for i in map:
+			 map[i][:]="-"
+def drawMap():
+	for i in map:
+			 print(i)
+#def randomStorage():
+#def randomPlayer():
+#def checkStorage():
+#def move():
+#def main():
+#	intMAp()
